084e65302a309af99bb466a25e4f040c3c382c2b
Maneesh1234 <mk049076@gmail.com>
2024-01-09 11:29:41 +0530

    [Maneesh] Modified: Comparision Length of the Line Using Equals Method

--- src/com/linecomp/Line.java
+++ src/com/linecomp/Line.java
@@ -1,7 +1,5 @@
 package com.linecomp;
 
-import java.util.Calendar;
-
 class Point{
 	 int x;
 	 int y;
@@ -20,10 +18,26 @@ public class Line {
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
-		Point p1=new Point(1,2);
-		Point p2=new Point(2,2);
+		Point p1=new Point(1,3);
+		Point p2=new Point(0,4);
 		Line l=new Line(p1,p2);
 		System.out.println("Length of Line is : "+ l.calculateLengthofLine());
+		Point p3=new Point(4,5);
+		Point p4=new Point(5,6);
+		Line l2=new Line(p3,p4);
+		if(l.checkEqualityOfLineUsingEquals(l2)){
+			System.out.println("Line are equal (Using Equals method)");
+		}
+		else {
+			System.out.println("Line are not equal (Using Equals method)");
+		}
+		
+	}
+	public boolean checkEqualityOfLineUsingEquals(Line l2) {
+		String lengthOffirstline=Double.valueOf(this.calculateLengthofLine()).toString();
+		String lengthOf2ndLine=Double.valueOf(l2.calculateLengthofLine()).toString();
+		return lengthOffirstline.equals(lengthOf2ndLine);
+		
 	}
 	public double calculateLengthofLine() {
 		int x1=this.p1.x;
